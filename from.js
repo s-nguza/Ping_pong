4763e1afcbe6c0b756d08078b6273bdc4c0df062
Siyabonga Nguza <159791703+s-nguza@users.noreply.github.com>
2024-11-15 10:23:40 +0200

    javascript code intial

---
+++ from.js
@@ -0,0 +1,8 @@
+
+//get the canvas and context
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerwidth;
+canvas.height = window.innerheight;
+
